b865f4f4a50c55661577d623dca87075457e7ccc
swemwave <ipktalha@gmail.com>
2026-02-10 13:07:19 -0700

    fix

--- app/week-5/item.tsx
+++ app/week-5/item.tsx
@@ -1,8 +1,8 @@
-export default function Item({ name, quantity, category }: ItemProps) {
+export default function Item({ name, quantity, category }: { name: string; quantity: number; category: string }) {
   return (
     <li className="border rounded p-3">
       <div className="font-semibold">{name}</div>
-      <div className="text-sm text-gray-400">
+      <div className="text-sm text-gray-">
         Buy {quantity} {name} in {category}
       </div>
     </li>
